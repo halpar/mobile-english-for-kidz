7e54ceb32cf12815f63eb6819ad708104c2f5034
alper sarper <halparsarper@gmail.com>
2020-07-25 18:21:38 +0300

    first commit for dev environment

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+export default class App extends Component {
+  render() {
+    return (
+      <LinearGradient>
+        colors={['#4c669f', '#3b5998', '#192f6a']}
+        style={styles.linearGradient}>
+        <Text style={styles.buttonText}>Sign in with Facebook</Text>
+      </LinearGradient>
+    );
+  }
+}
+
+// Later on in your styles..
+var styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  },
+});
